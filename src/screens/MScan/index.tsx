7aa5a011c4b2fb37fb9f18b66f0a9e555e62e066
minhtri1996 <trinm1996@gmail.com>
2021-01-06 23:19:30 +0700

    update

--- src/screens/MScan/index.tsx
+++ src/screens/MScan/index.tsx
@@ -1,13 +1,57 @@
 import React, {memo, useCallback, useState} from 'react';
-import {ScreenWrapper} from '@/themes/BaseStyles';
+import {BaseStyles, ScreenWrapper, SIcon} from '@/themes/BaseStyles';
 import {HeaderBack} from '@/components/HeaderBack';
+import {IC_SCAN} from '@/assets';
+import {Colors} from '@/themes/Colors';
+import {StyleSheet, Text, View} from 'react-native';
+import {styled} from '@/global';
+import SubmitButtonColor from '@/components/button/ButtonSubmit';
 
 export const MScanScreen = memo(function MScanScreen() {
+  const openSelectImage = useCallback(() => {}, []);
+
   return (
     <ScreenWrapper>
       <HeaderBack title={'MScan'} />
+      <SViewBody>
+        <View style={[BaseStyles.viewShadow, styles.viewCard]}>
+          <SText>Smart scan</SText>
+          <SImage size={200} tintColor={Colors.grey1} source={IC_SCAN} />
+          <SButton title={'Chọn ảnh'} onPress={openSelectImage} />
+        </View>
+      </SViewBody>
     </ScreenWrapper>
   );
+});
+
+const SText = styled.Text`
+  padding: 16px 0px;
+  align-self: center;
+  font-size: 24px;
+  color: ${Colors.grey3};
+`;
+
+const SViewBody = styled.View`
+  flex: 1;
+`;
+
+const SImage = styled.Image<{size?: number}>`
+  width: ${(props: any) => props.size || 24};
+  height: ${(props: any) => props.size || 24};
+  tint-color: ${(props: any) => props.tintColor || Colors.grey5};
+  align-self: center;
+`;
+
+const SButton = styled(SubmitButtonColor)`
+  padding: 0px 16px;
+`;
+
+const styles = StyleSheet.create({
+  viewCard: {
+    backgroundColor: '#f2f2f2',
+    borderRadius: 16,
+    margin: 16,
+  },
 });
 
 export default MScanScreen;
