6965a1aa279d79434c6691d317938540f34e288a
trí nguyễn <tri.nguyen@base.vn>
2021-03-26 10:49:54 +0700

    ud

--- src/screens/Login/Preload.tsx
+++ src/screens/Login/Preload.tsx
@@ -1,11 +1,12 @@
 import React, {memo, useCallback, useEffect} from 'react';
 import {styled} from '@/global';
 import {Colors} from '@/themes/Colors';
-import {ActivityIndicator} from 'react-native';
-import {navigateToFaceDetectScreen, navigateToLoginScreen} from '@/utils/navigation';
+import * as Animatable from 'react-native-animatable';
+import {navigateToLoginScreen} from '@/utils/navigation';
 import SplashScreen from 'react-native-splash-screen';
 import {IC_LOGO_APP} from '@/assets';
-
+import {Dimensions} from 'react-native';
+const {height, width} = Dimensions.get('window');
 const Container = styled.View`
   flex: 1;
   background-color: ${Colors.white};
@@ -13,16 +14,15 @@ const Container = styled.View`
   justify-content: center;
 `;
 
-const Logo = styled.Image`
+const Logo = styled(Animatable.Image)`
   width: 80px;
   height: 80px;
   border-radius: 120px;
-  background-color: #0077cc;
+  background-color: ${Colors.grey5};
 `;
 export const PreloadScreen = memo(function PreloadScreen() {
-  const getData = useCallback( () => {
+  const getData = useCallback(() => {
     setTimeout(() => {
-
       navigateToLoginScreen();
       // navigateToFaceDetectScreen();
     }, 500);
@@ -33,12 +33,20 @@ export const PreloadScreen = memo(function PreloadScreen() {
   }, []);
 
   useEffect(() => {
-    getData()
+    getData();
   }, []);
   return (
     <Container>
-      <Logo source = {IC_LOGO_APP} />
-      <ActivityIndicator size={'large'} color={Colors.gray3} />
+      <Logo
+        animation={{
+          from: {translateY: height / 4 - 150},
+          to: {translateY: 500 - height},
+        }}
+        duration={450}
+        easing={(t: any) => Math.pow(t, 1.7)}
+        source={IC_LOGO_APP}
+        useNativeDriver
+      />
     </Container>
   );
 });
